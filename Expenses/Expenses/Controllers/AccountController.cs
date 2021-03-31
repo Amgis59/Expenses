e3fef8b2f9cd4009083bbd5969b5bac43824c3ff
Amgis59 <ola19_95@wp.pl>
2021-04-01 00:52:45 +0200

    BeforeAppServices

---
+++ Expenses/Expenses/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Expenses.DataAccess;
+using Expenses.DataAccess.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expenses.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IRepository<Account> accountRepository;
+
+        public AccountController(IRepository<Account> accountRepository)
+        {
+            this.accountRepository = accountRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Account> GetAllExpenses()
+        {
+            return this.accountRepository.GetAll();
+        }
+
+        [HttpGet]
+        [Route("accountId")]
+        public Account GetExpenseById(int accountId)
+        {
+            return this.accountRepository.GetById(accountId);
+        }
+    }
+}
